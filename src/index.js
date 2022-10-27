ed0a820ae992844a476d03b5cce8f3bdfaa6f3a6
WanYu <yosoywanyushih@gmail.com>
2022-10-27 16:21:49 +0800

    Set up Stripe

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { persistor } from "./store/store";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
