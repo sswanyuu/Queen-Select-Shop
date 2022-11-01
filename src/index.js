a3e85f5ad61b00c5f6c6e14bbbadb274a23bf284
WanYu <yosoywanyushih@gmail.com>
2022-11-01 15:04:46 +0800

    Set up GraphQL and ApolloClient

--- src/index.js
+++ src/index.js
@@ -7,19 +7,28 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/users.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartItemProvider } from "./contexts/cart-items.context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://crwn-clothing.com/",
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*wrap <App/> with BrowserRouter to  read URL and track navigation*/}
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartItemProvider>
-            <App />
-          </CartItemProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      {/*wrap <App/> with BrowserRouter to  read URL and track navigation*/}
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartItemProvider>
+              <App />
+            </CartItemProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
